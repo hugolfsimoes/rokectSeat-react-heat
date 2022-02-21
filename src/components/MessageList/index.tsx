256fd38b4716837c4416319ef15d5a9fa5cb6540
hugolfsimoes <hugolfsimoes@gmail.com>
2022-02-21 20:39:23 -0300

    feito o front-end da aplicacao

--- src/components/MessageList/index.tsx
+++ src/components/MessageList/index.tsx
@@ -1,4 +1,5 @@
 import {useEffect, useState} from 'react';
+import io from 'socket.io-client';
 import {api} from '../../services/api';
 import styles from './styles.module.scss';
 
@@ -9,11 +10,31 @@ type Message = {
     name: string;
     avatar_url: string;
   };
-
 };
+const messageQueue: Message[] = [];
+
+const socket = io('http://localhost:4000');
+
+socket.on('new_message', (newMessage: Message) => {
+  messageQueue.push(newMessage);
+});
 
 export function MessageList() {
   const [ messages, setMessages ] = useState<Message[]>([]);
+
+  useEffect(() => {
+    setInterval(() => {
+      if (messageQueue.length > 0) {
+        setMessages(prevState => [
+          messageQueue[ 0 ],
+          prevState[ 0 ],
+          prevState[ 1 ],
+        ].filter(Boolean));
+        messageQueue.shift();
+      }
+    }, 3000);
+  }, []);
+
 
   useEffect(() => {
     api.get<Message[]>('messages/last3').then(response => {
@@ -41,15 +62,22 @@ export function MessageList() {
       </svg>
 
       <ul className={styles.messageList}>
-        <li className={styles.message}>
-          <p className={styles.messageContent}>Não vejo a hora de começar esse evento, com certeza vai ser o melhor de todos os tempos, vamoo pra cima!</p>
-          <div className={styles.messageUser}>
-            <div className={styles.userImage}>
-              <img src="https://github.com/hugolfsimoes.png" alt="Hugo Simões" />
-            </div>
-            <span>Hugo Simões</span>
-          </div>
-        </li>
+        {
+          messages.map(message => {
+            return (
+              <li key={message.id} className={styles.message}>
+                <p className={styles.messageContent}>{message.text}</p>
+                <div className={styles.messageUser}>
+                  <div className={styles.userImage}>
+                    <img src={message.user.avatar_url} alt={message.user.name} />
+                  </div>
+                  <span>{message.user.name}</span>
+                </div>
+              </li>
+            );
+          })
+        }
+
       </ul>
     </div>
   );
